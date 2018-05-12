49c114eb5b2727cedf451a378760ab60d2c86d3c
Nuea <nueha.pimkanok@gmail.com>
2018-05-12 11:52:38 +0700

    Create a page admin login with separate between user.

--- admin_panel/routes/web.php
+++ admin_panel/routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin','AdminController@login');
+//Route::get('/admin','AdminController@login');
+Route::match(array('GET', 'POST'),'/admin','AdminController@login');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
